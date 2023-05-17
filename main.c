cf7ee69f6518a011f488c4f776b55b0f51ec2aad
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2023-05-17 21:09:34 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 int main(int argc, char *argv[]){
     char *filename = "database/login.bin";
-      
+    if (argc == 1)
+    {
+        // Menampilkan petunjuk cara penggunaan program
+        printf("\nCara Penggunaan: \n");
+        printf("Untuk Registrasi: ./Program registrasi\n");
+        printf("Untuk Login: ./Program username password\n\n");
+        return EXIT_SUCCESS;
+    }
     return 0;
 }
